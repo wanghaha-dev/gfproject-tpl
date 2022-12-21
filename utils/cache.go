b196517aa46b0ab26c71d87d3df9cea02b061fd0
wanghaha-dev <mail65656@163.com>
2022-12-21 15:10:30 +0800

    1

--- utils/cache.go
+++ utils/cache.go
@@ -19,7 +19,9 @@ func AddCache(r *ghttp.Request, key string, data interface{}) {
 // RespCache 从缓存里直接返回
 func RespCache(r *ghttp.Request, key string) bool {
 	// get cache
-	exists, _ := g.Redis().DoVar("EXISTS", key)
+	exists, err := g.Redis().DoVar("EXISTS", key)
+	GetErrExit(r, err)
+
 	if exists.Int() == 1 {
 		redisData, _ := g.Redis().DoVar("get", key)
 
@@ -37,7 +39,12 @@ func RespCache(r *ghttp.Request, key string) bool {
 // GetCacheData 返回缓存数据
 func GetCacheData(key string) (map[string]interface{}, error) {
 	// get cache
-	exists, _ := g.Redis().DoVar("EXISTS", key)
+	exists, err := g.Redis().DoVar("EXISTS", key)
+	if err != nil {
+		g.Log().Error(err)
+		return nil, err
+	}
+
 	if exists.Int() == 1 {
 		redisData, _ := g.Redis().DoVar("get", key)
 

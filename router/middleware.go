35f56464ef94308d71e16221702eb5e0476d75ce
wanghaha-dev <mail65656@163.com>
2023-03-27 10:04:27 +0800

    update

--- router/middleware.go
+++ router/middleware.go
@@ -1,63 +1,10 @@
 package router
 
 import (
-	"fmt"
-	"github.com/wanghaha-dev/gf/frame/g"
 	"github.com/wanghaha-dev/gf/net/ghttp"
 	"myproject/utils"
 	"myproject/utils/gjwt"
-	"strings"
 )
-
-// AuthMiddleware 权限校验中间件
-func AuthMiddleware(r *ghttp.Request) {
-	fmt.Println("method:", r.Method, "r.Router.Uri:", r.Router.Uri)
-	fmt.Println(fmt.Sprintf("%v>%v", r.Method, r.Router.Uri))
-
-	tokenData, err := gjwt.ParseToken(r.GetHeader("Authorization"))
-	if err != nil {
-		_ = r.Response.WriteJsonExit(g.Map{"code": 401, "msg": err})
-		return
-	} else {
-		if tokenData["username"] == "admin" {
-			r.Middleware.Next()
-			return
-		}
-
-		fmt.Println("tokenData:", tokenData)
-		userObj, err := g.Model("sys_user").Where("userId=?", tokenData["userId"]).FindOne()
-		utils.GetErrExit(r, err)
-
-		rolesString := userObj["roles"].String()
-		rolesSlice := strings.Split(rolesString, ",")
-		fmt.Println("roleSlice:", rolesString)
-
-		roleMenus, err := g.Model("sys_role_menu").Where("roleId in (?)", rolesSlice).FindAll()
-		utils.GetErrExit(r, err)
-
-		menuIds := utils.ResultStrSlice(roleMenus, "menuIds")
-
-		menus, err := g.Model("sys_menu").Where("menuId in (?)", menuIds).FindAll()
-		utils.GetErrExit(r, err)
-
-		authorities := utils.ResultStrSlice(menus, "authority")
-
-		method := r.Method
-		router := r.Router.Uri
-		curAuth := fmt.Sprintf("%v>%v", method, router)
-
-		if utils.HasString(curAuth, authorities) {
-			r.Middleware.Next()
-		} else {
-			//r.Middleware.Next()
-			_ = r.Response.WriteJsonExit(g.Map{
-				"code":    403,
-				"message": "没有权限访问",
-			})
-		}
-	}
-}
-
 
 // JwtAuthMiddleware jwt身份验证中间件
 func JwtAuthMiddleware(r *ghttp.Request) {
--- router/router.go
+++ router/router.go
@@ -1,12 +1,9 @@
 package router
 
 import (
-	"myproject/app/api"
-	"myproject/app/api/artlib"
-	"myproject/utils"
-
 	"github.com/wanghaha-dev/gf/frame/g"
 	"github.com/wanghaha-dev/gf/net/ghttp"
+	"myproject/app/api"
 )
 
 func init() {
@@ -16,95 +13,17 @@ func init() {
 		r.Middleware.Next()
 	})
 
-	s.Use(utils.CacheMiddleware)
+	//s.Use(utils.CacheMiddleware)
 
+	// 无需登录的
 	s.Group("/", func(group *ghttp.RouterGroup) {
 		group.ALL("/hello", api.Hello)
 
-		// alCountry
-		group.GET("/alCountry/:id/detail", artlib.AlCountry.DetailById)
-		group.DELETE("/alCountry/:id/delete", artlib.AlCountry.DeleteById)
-		group.GET("/alCountry/list", artlib.AlCountry.List)
-		group.GET("/alCountry/all", artlib.AlCountry.All)
-		group.POST("/alCountry/add", artlib.AlCountry.Add)
-		group.POST("/alCountry/:id/update", artlib.AlCountry.UpdateById)
+	})
 
-		// alPeriod
-		group.GET("/alPeriod/:id/detail", artlib.AlPeriod.DetailById)
-		group.DELETE("/alPeriod/:id/delete", artlib.AlPeriod.DeleteById)
-		group.GET("/alPeriod/list", artlib.AlPeriod.List)
-		group.GET("/alPeriod/all", artlib.AlPeriod.All)
-		group.POST("/alPeriod/add", artlib.AlPeriod.Add)
-		group.POST("/alPeriod/:id/update", artlib.AlPeriod.UpdateById)
+	// 需要登录才能访问的
+	s.Group("/", func(group *ghttp.RouterGroup) {
+		group.Middleware(JwtAuthMiddleware)
 
-		// alStyle
-		group.GET("/alStyle/:id/detail", artlib.AlStyle.DetailById)
-		group.DELETE("/alStyle/:id/delete", artlib.AlStyle.DeleteById)
-		group.GET("/alStyle/list", artlib.AlStyle.List)
-		group.GET("/alStyle/all", artlib.AlStyle.All)
-		group.POST("/alStyle/add", artlib.AlStyle.Add)
-		group.POST("/alStyle/:id/update", artlib.AlStyle.UpdateById)
-
-		// alArtist
-		group.GET("/alArtist/:id/detail", artlib.Alartist.DetailById)
-		group.DELETE("/alArtist/:id/delete", artlib.Alartist.DeleteById)
-		group.GET("/alArtist/list", artlib.Alartist.List)
-		group.GET("/alArtist/all", artlib.Alartist.All)
-		group.POST("/alArtist/add", artlib.Alartist.Add)
-		group.POST("/alArtist/:id/update", artlib.Alartist.UpdateById)
-		group.GET("/alArtist/:id/works", artlib.Alartist.ArtistWorks)
-		group.GET("/alArtist/:id/typesCount", artlib.Alartist.TypesCount)
-
-		// alMuseum
-		group.GET("/alMuseum/:id/detail", artlib.Almuseum.DetailById)
-		group.DELETE("/alMuseum/:id/delete", artlib.Almuseum.DeleteById)
-		group.GET("/alMuseum/list", artlib.Almuseum.List)
-		group.GET("/alMuseum/all", artlib.Almuseum.All)
-		group.POST("/alMuseum/add", artlib.Almuseum.Add)
-		group.POST("/alMuseum/:id/update", artlib.Almuseum.UpdateById)
-
-		// alStory
-		group.GET("/alStory/:id/detail", artlib.Alstory.DetailById)
-		group.DELETE("/alStory/:id/delete", artlib.Alstory.DeleteById)
-		group.GET("/alStory/list", artlib.Alstory.List)
-		group.GET("/alStory/all", artlib.Alstory.All)
-		group.POST("/alStory/add", artlib.Alstory.Add)
-		group.POST("/alStory/:id/update", artlib.Alstory.UpdateById)
-
-		// alWork
-		group.GET("/alWork/:id/detail", artlib.Alwork.DetailById)
-		group.GET("/alWork/:id/download/list", artlib.Alwork.Download)
-		group.DELETE("/alWork/:id/delete", artlib.Alwork.DeleteById)
-		group.GET("/alWork/list", artlib.Alwork.List)
-		group.GET("/alWork/all", artlib.Alwork.All)
-		group.POST("/alWork/add", artlib.Alwork.Add)
-		group.POST("/alWork/:id/update", artlib.Alwork.UpdateById)
-		group.GET("/alWork/firstByCode", artlib.Alwork.FirstByCode)
-		group.GET("/alWork/typesCount", artlib.Alwork.TypesCount)
-
-		// alWorkFiles
-		group.GET("/alWorkFiles/:id/detail", artlib.Alworkfiles.DetailById)
-		group.DELETE("/alWorkFiles/:id/delete", artlib.Alworkfiles.DeleteById)
-		group.GET("/alWorkFiles/list", artlib.Alworkfiles.List)
-		group.GET("/alWorkFiles/all", artlib.Alworkfiles.All)
-		group.POST("/alWorkFiles/add", artlib.Alworkfiles.Add)
-		group.POST("/alWorkFiles/:id/update", artlib.Alworkfiles.UpdateById)
-		group.GET("/alWorkFiles/:workId/dzi", artlib.Alworkfiles.DZI)
-
-		// alTheme
-		group.GET("/alTheme/:id/detail", artlib.Altheme.DetailById)
-		group.DELETE("/alTheme/:id/delete", artlib.Altheme.DeleteById)
-		group.GET("/alTheme/list", artlib.Altheme.List)
-		group.GET("/alTheme/all", artlib.Altheme.All)
-		group.POST("/alTheme/add", artlib.Altheme.Add)
-		group.POST("/alTheme/:id/update", artlib.Altheme.UpdateById)
-
-		// typeCodes
-		group.GET("/typeCodes/:id/detail", artlib.Typecodes.DetailById)
-		group.DELETE("/typeCodes/:id/delete", artlib.Typecodes.DeleteById)
-		group.GET("/typeCodes/list", artlib.Typecodes.List)
-		group.GET("/typeCodes/all", artlib.Typecodes.All)
-		group.POST("/typeCodes/add", artlib.Typecodes.Add)
-		group.POST("/typeCodes/:id/update", artlib.Typecodes.UpdateById)
 	})
 }
